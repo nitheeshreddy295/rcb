8c20522548e15de91e30e6e3bbb1307727db2cfd
Nitheesh <nitheeshreddy295@gmail.com>
2022-04-14 11:40:59 +0530

    addition file modified
    i

--- addition.js
+++ addition.js
@@ -3,6 +3,6 @@ function add()
 var a = document.getElementById("firstno").value
 var b = document.getElementById("secondno").value
 var result
-result = a+b
+result = parseInt(a) + parseInt(b)
 document.getElementById("result").value = result
 }
