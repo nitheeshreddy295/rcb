da5559579478ed536de25efb3a89846b34d38ce4
Nitheesh <nitheeshreddy295@gmail.com>
2022-04-14 11:29:37 +0530

    changes

---
+++ substraction.js
@@ -0,0 +1,6 @@
+fuction multiplication() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+result = a*b
+document.getElementById("result").value=result
+}
